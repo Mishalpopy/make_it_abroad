1ecf47f6ae301cac11ea8d8c604459d306245dcb
MishalPopy <c.mishal46@gmail.com>
2022-07-30 22:17:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+$("#typed").typed({
+  strings: [
+    "I Wish I Was In London",
+    "Sky Rocket Your Career",
+    "Comming Soon",
+    "Make It Abroad ",
+  ],
+  typeSpeed: 20,
+  startDelay: 0,
+  backSpeed: 20,
+  backDelay: 1000,
+  loop: true,
+  cursorChar: "|",
+  contentType: "html",
+});
